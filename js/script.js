07ce54e82e6a8718e924a5e6a89d1b4df23fd848
Oleg <savochkinoleg17@gmail.com>
2023-04-12 18:00:05 +0300

    build: adaptive

--- js/script.js
+++ js/script.js
@@ -3,25 +3,42 @@
 // if (window.matchMedia("(min-width: 1220px)").matches) {
 //     numOfSlides = 2
 // }
-
-$('.slider__inner').slick({
-    infinite: true,
-    draggable: true,
-    variableWidth: true,
-    dots: true,
-    arrows: true,
-    prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/prev.svg'>",
-    nextArrow:"<img class='a-right control-c next slick-next' src='../images/next.svg'>",
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    // mobileFirst:true,
-    // responsive: [{
-    //     breakpoint: 1335,
-    //     settings: {
-    //         slidesToShow: 2,
-    //         slidesToScroll: 2,
-    //         centerMode: false
-    //     }
-    // }]
+$(document).ready(function () {
+    $('.slider__inner').slick({
+        infinite: true,
+        dots: true,
+        arrows: true,
+        prevArrow: "<img class='a-left control-c prev slick-prev' src='../images/prev.svg'>",
+        nextArrow: "<img class='a-right control-c next slick-next' src='../images/next.svg'>",
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        // variableWidth: true,
+        // respondTo: true,
+        // adaptiveHeight: true,
+        responsive: [{
+            breakpoint: 1250,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                // variableWidth: true,
+            }
+        },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    // variableWidth: true,
+                }
+            }]
+    });
+    
+    $(".burger").click(function (event) {
+        // if(event.target.className.includes('burger')){
+        //
+        // }
+        $(".burger, .menu").toggleClass("active");
+        $("body").toggleClass("lock")
+    })
 });
-
