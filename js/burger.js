df86554cf2c2a2ee5bb22c0b9582b2323b6ada0a
Oleg <savochkinoleg17@gmail.com>
2023-04-13 09:53:15 +0300

    build: add navigation on page

--- js/burger.js
+++ js/burger.js
@@ -5,3 +5,8 @@ $(".burger").click(function (event) {
     $(".burger, .menu").toggleClass("active");
     $("body").toggleClass("lock")
 })
+
+// $(".menu").click(function () {
+//     $(".burger, .menu").removeClass("active");
+//     $("body").removeClass("lock")
+// })
